1fa8d02ccd4cb857dafc8d19be1d4f1816ebb94c
Nicolas Boutet <amd3002@gmail.com>
2023-04-30 11:56:40 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,21 +5,28 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('util-linux'),
-  ('procps'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("util-linux"),
+        ("procps"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('file,user,group,mode', [
-  ('/swapfile_test', 'root', 'root', 0o600),
-])
+@pytest.mark.parametrize(
+    "file,user,group,mode",
+    [
+        ("/swapfile_test", "root", "root", 0o600),
+    ],
+)
 def test_swap_file_exist(host, file, user, group, mode):
     swap = host.file(file)
     assert swap.exists
@@ -29,10 +36,13 @@ def test_swap_file_exist(host, file, user, group, mode):
     assert swap.mode == mode
 
 
-@pytest.mark.parametrize('key,expected_value', [
-  ('vm.swappiness', 10),
-  ('vm.vfs_cache_pressure', 400),
-])
+@pytest.mark.parametrize(
+    "key,expected_value",
+    [
+        ("vm.swappiness", 10),
+        ("vm.vfs_cache_pressure", 400),
+    ],
+)
 def test_sysctl_values(host, key, expected_value):
     value = host.sysctl(key)
     assert expected_value == value
